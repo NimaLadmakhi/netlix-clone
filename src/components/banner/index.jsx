e06dc16837ceebf387bc1ebb18da6925c11a5d9e
nima ladmakhi <cody18v@gmail.com>
2021-02-01 01:37:50 -0800

    add youtube player

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -2,6 +2,7 @@
 
 import { ImageDB_URL, MovieDB_URL, request } from 'fetcher';
 import React from 'react';
+import YouTube from 'react-youtube';
 import { useRecoilValue } from 'recoil';
 import { TRENDING_MOVIES_ATOM } from 'recoil/atoms';
 import './index.scss';
@@ -19,21 +20,24 @@ const Banner = () => {
           return () => clearInterval(trendingInterval);
      }, [currentTrendingIndex]);
 
-     const YoutubeHandler = async () => {
+     const youtubeHandler = async () => {
           const currentTrendingMovie = trendingMovie[currentTrendingIndex];
           const response = await request(MovieDB_URL(currentTrendingMovie.id));
           setCurrentKeyMovie(response?.results[0]?.key);
      };
-
+     
+     const resetYoutubeKey = () => setCurrentKeyMovie();
      const currentMovie = trendingMovie[currentTrendingIndex];
-
+     if (currentKeyMovie) return <YouTube videoId={currentKeyMovie} className='youtube__video' onEnd={resetYoutubeKey} />;
      return (
-          <div className='banner'>
-               <img className='banner__image' src={ImageDB_URL(currentMovie.backdrop_path)} />
-               <div className='banner__content'>
-                    <h1 className='banner__title'>{currentMovie.title}</h1>
-                    <p className='banner__description'>{currentMovie.overview}</p>
-               </div>
+          <div className='banner' onClick={youtubeHandler}>
+               <p>
+                    <img className='banner__image' src={ImageDB_URL(currentMovie.backdrop_path)} />
+                    <div className='banner__content'>
+                         <h1 className='banner__title'>{currentMovie.title}</h1>
+                         <p className='banner__description'>{currentMovie.overview}</p>
+                    </div>
+               </p>
           </div>
      );
 };
